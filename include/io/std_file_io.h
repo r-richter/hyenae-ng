3a3b272f9f79182f7d95e6f56c7d8c8b576c4093
Hyenae-NG <hyenae.tool@googlemail.com>
2020-06-23 13:19:24 +0200

    implemented file access abstraction layer

---
+++ include/io/std_file_io.h
@@ -0,0 +1,57 @@
+/*
+ * Hyenae NG
+ *   Advanced Network Packet Generator (NextGen)
+ *
+ * Copyright (C) 2020 Robin Richter
+ *
+ *   Contact  : hyenae.tool@googlemail.com
+ *   Homepage : https://github.com/r-richter/hyenae-ng
+ *
+ * This file is part of Hyenae NG.
+ *
+ * Hyenae NG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hyenae NG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hyenae NG.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef STD_FILE_IO_H
+#define STD_FILE_IO_H
+
+#include "../file_io.h"
+
+#include <fstream>
+
+namespace hyenae::io
+{
+    /*---------------------------------------------------------------------- */
+
+	class std_file_io :
+		public file_io
+	{
+		private:
+            std::fstream _stream;
+			
+		public:
+			bool is_open() const;
+			void open(const string_t& filename, bool overwrite);
+			void close() noexcept;
+			void write(const string_t& content);
+			string_t read_all();
+
+	}; /* std_file_io */
+
+    /*---------------------------------------------------------------------- */
+
+} /* hyenae::io */
+
+#endif /* STD_FILE_IO_H */
---
+++ src/io/std_file_io.cpp
@@ -0,0 +1,83 @@
+/*
+ * Hyenae NG
+ *   Advanced Network Packet Generator (NextGen)
+ *
+ * Copyright (C) 2020 Robin Richter
+ *
+ *   Contact  : hyenae.tool@googlemail.com
+ *   Homepage : https://github.com/r-richter/hyenae-ng
+ *
+ * This file is part of Hyenae NG.
+ *
+ * Hyenae NG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hyenae NG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hyenae NG.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "../include/assert.h"
+#include "../include/io/std_file_io.h"
+
+namespace hyenae::io
+{
+    /*---------------------------------------------------------------------- */
+
+    bool std_file_io::is_open() const
+    {
+        return _stream.is_open();
+
+    } /* is_open */
+
+    /*---------------------------------------------------------------------- */
+
+    void std_file_io::open(const string_t& filename, bool overwrite)
+    {
+        assert::legal_call(!is_open(), "", "already open");
+
+        _stream.open(
+            filename, overwrite ? std::ios_base::trunc : std::ios_base::app);
+
+    } /* open */
+
+    /*---------------------------------------------------------------------- */
+
+    void std_file_io::close() noexcept
+    {
+        _stream.close();
+
+    } /* close */
+
+    /*---------------------------------------------------------------------- */
+
+    void std_file_io::write(const string_t& content)
+    {
+        assert::legal_call(is_open(), "", "not open");
+
+        _stream.write(content.c_str(), content.size());
+
+    } /* write */
+
+    /*---------------------------------------------------------------------- */
+
+    string_t std_file_io::read_all()
+    {
+        assert::legal_call(is_open(), "", "not open");
+
+        return string_t(
+            std::istreambuf_iterator<char>(_stream),
+            std::istreambuf_iterator<char>());
+            
+    } /* read_all */
+
+    /*---------------------------------------------------------------------- */
+
+} /* hyenae::io */
